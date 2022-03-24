32b837140903058e764673998ec473ca8ffae811
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-24 20:19:46 +0100

    Final Push

--- main.js
+++ main.js
@@ -18,20 +18,18 @@ console.log(età_passeggiero, numero_km);
 
 // calcolare il prezzo del biglietto
 let prezzo_biglietto_intero = numero_km * 0.21
-console.log(prezzo_biglietto_intero)
+let prezzo_finale = prezzo_biglietto_intero
 
 //sconti
 
 if (età_passeggiero < 18){
     const sconto_minorenne = prezzo_biglietto_intero / 100 * 20;
-    const prezzo_finale = prezzo_biglietto_intero - sconto_minorenne;
+    prezzo_finale = prezzo_biglietto_intero - sconto_minorenne;
     console.log(prezzo_finale);
 } else if (età_passeggiero > 65){
     const sconto_anziano = prezzo_biglietto_intero / 100 * 40;
-    const prezzo_finale = prezzo_biglietto_intero - sconto_anziano;
+    prezzo_finale = prezzo_biglietto_intero - sconto_anziano;
     console.log(prezzo_finale);
-} else {
-  console.log(prezzo_biglietto_intero)
-}
+} 
 
-document.getElementById("prezzo").innerHTML = prezzo_biglietto_intero.toFixed(2)
+document.getElementById("prezzo").innerHTML = prezzo_finale.toFixed(2)
